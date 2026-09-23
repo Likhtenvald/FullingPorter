a84626eba649ebcd88763d63e45f06570aabd2c5
Likhtenvald <44159900+Likhtenvald@users.noreply.github.com>
2026-09-23 13:30:50 +0600

    feat: localize single porter limit

--- src/Localization/LocalizationRegistry.cs
+++ src/Localization/LocalizationRegistry.cs
@@ -14,6 +14,7 @@ internal static class LocalizationRegistry
         ["fullingporter_source_enabled"] = "Porter source chest assigned",
         ["fullingporter_source_disabled"] = "Porter source chest unassigned",
         ["fullingporter_spawned"] = "Your Fuling porter has arrived",
+        ["fullingporter_already_exists"] = "Only one porter can be contracted in this world",
         ["fullingporter_rename_title"] = "Name your porter",
         ["fullingporter_dismissed"] = "Porter contract terminated",
         ["fullingporter_dismiss_confirm"] = "Press the dismissal key again within 3 seconds to terminate this contract"
@@ -28,6 +29,7 @@ internal static class LocalizationRegistry
         ["fullingporter_source_enabled"] = "Сундук назначен источником грузчика",
         ["fullingporter_source_disabled"] = "Сундук больше не является источником грузчика",
         ["fullingporter_spawned"] = "Ваш фулинг-грузчик прибыл",
+        ["fullingporter_already_exists"] = "В этом мире уже есть фулинг-грузчик",
         ["fullingporter_rename_title"] = "Имя грузчика",
         ["fullingporter_dismissed"] = "Контракт с грузчиком расторгнут",
         ["fullingporter_dismiss_confirm"] = "Нажмите клавишу увольнения ещё раз в течение 3 секунд, чтобы расторгнуть контракт"
