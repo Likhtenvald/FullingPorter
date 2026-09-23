2dc5c039e95baa1c841a3367f9705ca065ef7049
Likhtenvald <44159900+Likhtenvald@users.noreply.github.com>
2026-09-23 09:55:36 +0600

    feat: add playable porter interaction foundations

--- src/Localization/LocalizationRegistry.cs
+++ src/Localization/LocalizationRegistry.cs
@@ -1,4 +1,5 @@
 using Jotunn.Managers;
+using System.Collections.Generic;
 
 namespace FullingPorter.Localization;
 
@@ -6,19 +7,25 @@ internal static class LocalizationRegistry
 {
     internal static void Register()
     {
-        LocalizationManager.Instance.AddTranslation("English", new()
+        LocalizationManager.Instance.AddTranslation("English", new Dictionary<string, string>
         {
             ["fullingporter_name"] = "Fuling Porter",
             ["fullingporter_interact"] = "Talk",
             ["fullingporter_contract"] = "Fuling Porter Contract",
-            ["fullingporter_contract_desc"] = "A contract for the services of a surprisingly disciplined Fuling porter."
+            ["fullingporter_contract_desc"] = "A contract for the services of a surprisingly disciplined Fuling porter.",
+            ["fullingporter_source_enabled"] = "Porter source chest assigned",
+            ["fullingporter_source_disabled"] = "Porter source chest unassigned",
+            ["fullingporter_spawned"] = "Your Fuling porter has arrived"
         });
-        LocalizationManager.Instance.AddTranslation("Russian", new()
+        LocalizationManager.Instance.AddTranslation("Russian", new Dictionary<string, string>
         {
             ["fullingporter_name"] = "Фулинг-грузчик",
             ["fullingporter_interact"] = "Поговорить",
             ["fullingporter_contract"] = "Контракт фулинга-грузчика",
-            ["fullingporter_contract_desc"] = "Контракт на услуги на удивление дисциплинированного фулинга-грузчика."
+            ["fullingporter_contract_desc"] = "Контракт на услуги на удивление дисциплинированного фулинга-грузчика.",
+            ["fullingporter_source_enabled"] = "Сундук назначен источником грузчика",
+            ["fullingporter_source_disabled"] = "Сундук больше не является источником грузчика",
+            ["fullingporter_spawned"] = "Ваш фулинг-грузчик прибыл"
         });
     }
 }
